31879ae604d90b737f60017b328159fb5fcfa80a
Jefri Mustapa <jefri.mustapa@gmail.com>
2026-08-28 08:26:39 +0800

    feat(series): display episode release air date and grayscale unaired episode posters in details page

--- src/version.ts
+++ src/version.ts
@@ -2,8 +2,8 @@
 export type BuildChannel = 'dev' | 'nightly' | 'rc' | 'stable' | 'ci';
 
 export const APP_VERSION = '1.2.0';
-export const APP_BUILD_NUMBER = 239;
-export const APP_VERSION_FULL = '1.2.0-dev.jefri.fix-details-page-ref.239';
+export const APP_BUILD_NUMBER = 240;
+export const APP_VERSION_FULL = '1.2.0-dev.jefri.feat-series-episode-.240';
 export const APP_BUILD_CHANNEL: BuildChannel = 'dev';
 export const APP_BUILD_DEV = 'jefri';
-export const APP_BRANCH = 'fix-details-page-ref';
+export const APP_BRANCH = 'feat-series-episode-';
