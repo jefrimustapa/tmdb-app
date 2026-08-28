801f3a0515972b738e6c2d1e74a9f1d3fb73d719
Jefri Mustapa <jefri.mustapa@gmail.com>
2026-08-28 17:21:31 +0800

    feat(providers): add 23 new streaming provider mirrors based on LunaStream

--- src/version.ts
+++ src/version.ts
@@ -2,8 +2,8 @@
 export type BuildChannel = 'dev' | 'nightly' | 'rc' | 'stable' | 'ci';
 
 export const APP_VERSION = '1.2.0';
-export const APP_BUILD_NUMBER = 271;
-export const APP_VERSION_FULL = '1.2.0-dev.jefri.fix-watch-headerbar-.271';
+export const APP_BUILD_NUMBER = 273;
+export const APP_VERSION_FULL = '1.2.0-dev.jefri.feat-add-lunastream-.273';
 export const APP_BUILD_CHANNEL: BuildChannel = 'dev';
 export const APP_BUILD_DEV = 'jefri';
-export const APP_BRANCH = 'fix-watch-headerbar-';
+export const APP_BRANCH = 'feat-add-lunastream-';
